39c915512529bf05544353a52fee0883315f7d4d
mertaren <mertcodes@gmail.com>
2025-12-13 11:23:16 +0300

    add: Launch file and base launch.py

---
+++ warehouse_simulation/launch/main_simulation.launch.py
@@ -0,0 +1,21 @@
+import os
+import launch 
+import launch.actions
+import launch_ros.actions
+from ament_index_python import get_package_share_directory
+
+def generate_launch_description():
+    '''
+    Docstring for generate_launch_description
+    '''
+    # 1 package paths
+    # 2 include
+    # 3 robot model
+    # 4 state publisher
+    # 5 spawn
+    
+    return launch.LaunchDescription([
+        #cmd
+        #publisher
+        #spawn
+    ])
