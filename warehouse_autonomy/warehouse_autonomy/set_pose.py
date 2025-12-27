1a13bc265aa77c18366d92755fea0d142e0611c8
mertaren <mertcodes@gmail.com>
2025-12-27 19:24:37 +0300

    fix(localization): Implement robust initial pose publishing with retry logic
    - Replaced unreliable 'fire-and-forget' logic with a timer-based publisher
    - Added 3-attempt retry mechanism to ensure AMCL receives the pose
    - Solved race condition where the node exited before message delivery
    - Validated correct coordinates matching Gazebo spawn point

--- warehouse_autonomy/warehouse_autonomy/set_pose.py
+++ warehouse_autonomy/warehouse_autonomy/set_pose.py
@@ -7,6 +7,8 @@ class InitialPosePublisher(Node):
         super().__init__('initial_pose_publisher')
         self.publisher_ = self.create_publisher(PoseWithCovarianceStamped, '/initialpose', 10)
         
+        self.counter = 0
+        
         self.timer = self.create_timer(1.0, self.publish_pose)
         self.get_logger().info('Initial Pose Publisher Preparing...')
 
@@ -16,30 +18,33 @@ class InitialPosePublisher(Node):
         msg.header.stamp = self.get_clock().now().to_msg()
         
        
-        msg.pose.pose.position.x = 0.0499706
-        msg.pose.pose.position.y = -0.183695
+        msg.pose.pose.position.x = 0.1578
+        msg.pose.pose.position.y = -0.17247
         msg.pose.pose.position.z = 0.0
         
         
         msg.pose.pose.orientation.x = 0.0
         msg.pose.pose.orientation.y = 0.0
-        msg.pose.pose.orientation.z = -0.00271926
-        msg.pose.pose.orientation.w = 0.999996
+        msg.pose.pose.orientation.z = 0.00035
+        msg.pose.pose.orientation.w = 0.99999
 
         # Covariance matris
         msg.pose.covariance = [
-            0.25, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.25, 0.0, 0.0, 0.0, 0.0,
+            0.1991521, 0.0004791921160253107, 0.0, 0.0, 0.0, 0.0,
+            0.0004791921160253107, 0.23625413334343975, 0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
             0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
-            0.0, 0.0, 0.0, 0.0, 0.0, 0.06853891945200942
+            0.0, 0.0, 0.0, 0.0, 0.0, 0.05757644606312473
         ]
 
         self.publisher_.publish(msg)
-        self.get_logger().info('Initial Pose Sent Closing..')
-        # Execute 
-        raise SystemExit
+        self.counter += 1
+        
+        if self.counter >= 3:
+            self.get_logger().info('Initial Pose Sent Closing..')
+            # Execute 
+            raise SystemExit
 
 def main(args=None):
     rclpy.init(args=args)
@@ -47,6 +52,7 @@ def main(args=None):
     try:
         rclpy.spin(node)
     except SystemExit:
+        node.destroy_node()
         rclpy.shutdown()
 
 if __name__ == '__main__':
